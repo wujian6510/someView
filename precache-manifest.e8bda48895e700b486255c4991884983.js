6a495f8c83cb228f7126d4a13c6fb150136b7f58
wujian <wu.jian92@iwhalecloud.com>
2019-08-06 10:43:47 +0800

    add 初始化

---
+++ precache-manifest.e8bda48895e700b486255c4991884983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3769d28d090346c0e6b4",
+    "url": "/static/js/main.3769d28d.chunk.js"
+  },
+  {
+    "revision": "3f2b0c361f84a06854f7",
+    "url": "/static/js/1.3f2b0c36.chunk.js"
+  },
+  {
+    "revision": "3769d28d090346c0e6b4",
+    "url": "/static/css/main.ba84f91a.chunk.css"
+  },
+  {
+    "revision": "567f817cef0f8d03debc4d54b433aa2a",
+    "url": "/index.html"
+  }
+];
